908cb7c817023f10dbef5ba76d6dd7274ac9e5a2
Yuriy Sverchkov <yuriy@biostat.wisc.edu>
2016-09-12 14:27:12 -0500

    Added NEM settings

--- R/ferm-nem.R
+++ R/ferm-nem.R
@@ -4,10 +4,18 @@ load( "clean-data/ferm-for-nem.RData" )
 
 D = getDensityMatrix( ferm.p, dirname="tmp" )
 
+# getDensityMatrix forgets row names for some reason
 dimnames( D ) = dimnames( ferm.p )
 
 save( D, file = "intermediates/ferm.RData" )
 
-ferm.nem = nem( D )
+# NEM code can't handle infinities
+D[ D == -Inf ] = -.Machine$double.xmax
+
+library( nem )
+nem.control = set.default.parameters( colnames( D ) )
+nem.control$type = "CONTmLLBayes"
+  
+ferm.nem = nem( D, inference = "triples", control = nem.control )
 
 save( ferm.nem, file = "results/ferm.RData" )
